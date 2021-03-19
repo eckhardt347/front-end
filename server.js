b8f1b755e527a4b3c95f9651ba2380aa35812efc
matthew-eckhardt <matthechief97@gmail.com>
2021-03-19 12:38:08 +0000

    v2 corrected errors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var request      = require("request")
 
 
 global.acmws = {};
-  
+
 global.acmws['request-latency'] = 0;
 global.acmws['request-latency-catalogue'] = 500;
 global.acmws['response-error-probability'] = 0;
